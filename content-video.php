952211eb67ff2708f118d1a5bede983a448c8c55
kLOsk <kloskillah@gmx.net>
2014-08-02 20:36:51 +0900

    Fixed smaller bugs and added some fancyness

--- content-video.php
+++ content-video.php
@@ -16,10 +16,6 @@
 		</div>
 		<?php if ( is_single() ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
 		<?php endif; // is_single() ?>
 		<?php if ( is_single() ) : ?> <div class="entry-meta">
 			<?php lifeisgood_entry_meta(); ?>

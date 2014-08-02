9b53119020e785e34459df94251526291ee9b127
kLOsk <kloskillah@gmx.net>
2014-08-03 07:30:28 +0900

    Front Title Changes and Screenshot

--- content-audio.php
+++ content-audio.php
@@ -16,13 +16,7 @@
 		</div>
 		<?php endif; ?>
 
-		<?php if ( is_single() ) : ?>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
-		<?php endif; // is_single() ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
